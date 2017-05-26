20f4bd5678e0c4b6396717a8234176c00fe21119
tjh581 <thomas1hill@gmail.com>
2017-05-25 23:31:07 -0400

    adding comments

--- read_moisture.py
+++ read_moisture.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python
 import time
 
+#import ADC code
 import Adafruit_ADS1x15
 
+#import numpy for median
 import numpy
 
+#create a function to reutrn the soil moisture
 def read_moisture():
     #Create an ADS1015 ADC (12-bit) instance
     adc = Adafruit_ADS1x15.ADS1015()
@@ -13,6 +16,7 @@ def read_moisture():
 
     readings = []
 
+    #take 5 readings .5 seconds apart and store in list
     for i in range(5):
         readings.append(adc.read_adc(0, gain=GAIN))
         time.sleep(0.5)
