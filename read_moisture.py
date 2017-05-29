a9c70fc30d7b7b7ebefabca92a548f3297e2b38e
tjh581 <thomas1hill@gmail.com>
2017-05-29 19:41:55 -0400

    pass channel in function

--- read_moisture.py
+++ read_moisture.py
@@ -7,8 +7,8 @@ import Adafruit_ADS1x15
 #import numpy for median
 import numpy
 
-#create a function to reutrn the soil moisture
-def read_moisture():
+#create a function to reutrn the soil moisture; x = channel
+def read_moisture(x):
     #Create an ADS1015 ADC (12-bit) instance
     adc = Adafruit_ADS1x15.ADS1015()
 
@@ -18,11 +18,11 @@ def read_moisture():
 
     #take 5 readings .5 seconds apart and store in list
     for i in range(5):
-        readings.append(adc.read_adc(0, gain=GAIN))
+        readings.append(adc.read_adc(x, gain=GAIN))
         time.sleep(0.5)
 
     soil = numpy.median(readings)
 
     return soil
 
-print read_moisture()
+print read_moisture(0)
